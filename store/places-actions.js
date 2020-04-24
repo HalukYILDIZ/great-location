21498e2c6ae96941feaee249e0a54f9d3cd369c1
Haluk YILDIZ <halukyildiz4000@gmail.com>
2020-04-24 16:18:42 +0300

    sql db init and insert func added

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,6 +1,7 @@
 import * as FileSystem from "expo-file-system";
 
 export const ADD_PLACE = "ADD_PLACE";
+import { insertPlace } from "../helpers/db";
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
@@ -11,11 +12,26 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath,
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "Dummy daress",
+        15.6,
+        15.4
+      );
+      console.log(dbResult);
+      dispatch({
+        type: ADD_PLACE,
+        placeData: { title: title, image: newPath },
+      });
     } catch (err) {
       console.log(err);
       throw err;
     }
 
-    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+    dispatch({
+      type: ADD_PLACE,
+      placeData: { id: dbResult.insertId, title: title, image: newPath },
+    });
   };
 };
