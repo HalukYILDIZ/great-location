49b9a528ff017b768a7ff8be33fac3cb29af5530
Haluk YILDIZ <halukyildiz4000@gmail.com>
2020-04-26 13:58:23 +0300

    SQLite bug fixed

--- store/places-actions.js
+++ store/places-actions.js
@@ -4,11 +4,25 @@ export const ADD_PLACE = "ADD_PLACE";
 export const SET_PLACES = "SET_PLACES";
 
 import { insertPlace, fetchPlaces } from "../helpers/db";
+import ENV from "../env";
 
-export const addPlace = (title, image) => {
+export const addPlace = (title, image, location) => {
   return async (dispatch) => {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${ENV.googleApiKey}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    const resData = await response.json();
+    if (!resData.results) {
+      throw new Error("Something went wrong");
+    }
+    const address = resData.results[0].formatted_address;
     const fileName = image.split("/").pop();
     const newPath = FileSystem.documentDirectory + fileName;
+
     try {
       await FileSystem.moveAsync({
         from: image,
@@ -17,14 +31,20 @@ export const addPlace = (title, image) => {
       const dbResult = await insertPlace(
         title,
         newPath,
-        "Dummy daress",
-        15.6,
-        15.4
+        address,
+        location.lat,
+        location.lng
       );
 
       dispatch({
         type: ADD_PLACE,
-        placeData: { id: dbResult.insertId, title: title, image: newPath },
+        placeData: {
+          id: dbResult.insertId,
+          title: title,
+          image: newPath,
+          address: address,
+          coords: { lat: location.lat, lng: location.lng },
+        },
       });
     } catch (err) {
       console.log(err);
